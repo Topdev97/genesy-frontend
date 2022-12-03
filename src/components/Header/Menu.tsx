448e656c1859818138cbf589356bb52ea9bc9642
ScriptSavior <laoroney1113@gmail.com>
2022-12-03 23:40:04 +0900

    user_profile_add

--- src/components/Header/Menu.tsx
+++ src/components/Header/Menu.tsx
@@ -1,14 +1,46 @@
-import React, { useEffect } from "react";
-
-const Menu = () => {
-  const menuItem: string[] = ["Create", "Profile", "Sign out"];
+import React, { useEffect, Dispatch, SetStateAction } from "react";
+import LinkWithSearchParams from "../LinkWithSearchParams";
+type AppProps = {
+  onDisconnectWallet: () => Promise<void>;
+  setIsMenu: Dispatch<SetStateAction<boolean>>;
+};
+const Menu = (props: AppProps) => {
   return (
     <div className="absolute top-12 bg-white  w-32 right-0 menu-shadow">
-      {menuItem.map((item) => (
-        <div key={item} className="px-4 py-2 hover:bg-gray-200">
-          {item}
+      <LinkWithSearchParams
+        to={{
+          pathname: "/mint",
+        }}
+      >
+        <div
+          className="px-4 py-2 hover:bg-gray-200"
+          onClick={() => props.setIsMenu(false)}
+        >
+          Create
         </div>
-      ))}
+      </LinkWithSearchParams>
+
+      <LinkWithSearchParams
+        to={{
+          pathname: "/profile/:address/*",
+        }}
+      >
+        <div
+          className="px-4 py-2 hover:bg-gray-200"
+          onClick={() => props.setIsMenu(false)}
+        >
+          Profile
+        </div>
+      </LinkWithSearchParams>
+      <div
+        className="px-4 py-2 hover:bg-gray-200"
+        onClick={() => {
+          props.onDisconnectWallet();
+          props.setIsMenu(false);
+        }}
+      >
+        Sign out
+      </div>
     </div>
   );
 };

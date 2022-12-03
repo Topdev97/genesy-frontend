91c932a5313250b1aecf0d67ac1909f2acc48872
ScriptSavior <laoroney1113@gmail.com>
2022-12-03 23:25:30 +0900

    footer_update

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,19 +1,32 @@
 import React, { useEffect } from "react";
 import { FiTwitter } from "react-icons/fi";
+import LinkWithSearchParams from "../LinkWithSearchParams";
 const Footer = () => {
   return (
     <div className=" bg-black">
       <div className=" text-white flex justify-between py-12 h-60 max-w-[1024px] mx-auto sm:px-8 lg:px-0">
         <div className="flex flex-col justify-between">
-          <div className="text-xl border-r-8 border-white pr-4 ">GENESY</div>
+          <LinkWithSearchParams
+            to={{
+              pathname: "/",
+            }}
+          >
+            <div className="text-xl border-r-8 border-white pr-4 ">GENESY</div>
+          </LinkWithSearchParams>
           <div className="text-sm">
             &#169;{new Date().getFullYear()} Genesys Terms and conditions
           </div>
         </div>
         <div className="flex flex-col justify-between text-right text-sm">
           <div className="flex flex-col gap-4">
-            <div>FAQ</div>
-            <div>US@GENESY.XYZ</div>
+            <LinkWithSearchParams
+              to={{
+                pathname: "/faq",
+              }}
+            >
+              FAQ
+            </LinkWithSearchParams>
+            <a href="mailto: US@GENESY.XYZ">US@GENESY.XYZ</a>
           </div>
           <div className="flex flex-col gap-4">
             <div>FOLLOW US</div>
